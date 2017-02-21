58623af2b9bd693e98f5e0c860640c64fcb51e2a
JeremyTCD <jeremytanchongdao@gmail.com>
2017-02-21 15:16:25 +0800

    Finished up build.dev.js

--- build.dev.js
+++ build.dev.js
@@ -1,11 +1,12 @@
-const webpack = require('webpack');
-const argv = require('yargs').argv;
 const path = require('path');
+const argv = require('yargs').argv;
+
 const templateProjectDir = argv.t;
+
+// Start webpack-dev-server
+const webpack = require('webpack');
 const webpackDevServer = require('webpack-dev-server');
 const templateWebpackConfig = require(path.join(templateProjectDir, 'webpack.config.js'));
-
-// Add client/index.js to bundle
 templateWebpackConfig.entry['styles/bundle'].unshift("webpack-dev-server/client?http://localhost:8080/")
 templateWebpackConfig.resolve.modules.unshift(path.join(__dirname, "node_modules"));
 const compiler = webpack(templateWebpackConfig);
@@ -15,10 +16,23 @@ const server = new webpackDevServer(compiler,
         watchContentBase: true,
         publicPath: '/'
     });
-
 server.listen(8080, "127.0.0.1", function () {
     console.log("Starting server on http://localhost:8080");
 });
 
-// TODO: watch templates mustache template files and plugins, call 
-// docfx if these change.
+// Start docfx watcher
+const watch = require('watch');
+const execFileSync = require('child_process').execFileSync;
+const templateCopyFiles = require(path.join(templateProjectDir, 'build.copy.js'));
+var numRuns = 0;
+function docfxBuild(f, curr, previous) {
+    if (++numRuns > 3) {// Only run once on initialization
+        templateCopyFiles();
+        execFileSync('docfx', ['build', '-t ' + path.join(templateProjectDir, 'bin')], { stdio: [0, 1, 2] });
+    }
+}
+watch.watchTree(path.join(templateProjectDir, 'templates'), docfxBuild);
+watch.watchTree(path.join(templateProjectDir, 'plugins'), docfxBuild);
+watch.watchTree(path.join(templateProjectDir, 'fonts'), docfxBuild);
+watch.watchTree(path.join(templateProjectDir, 'misc'), docfxBuild);
+
