9fd1f9881c915237f58b8c29620b8d0221e303b9
JeremyTCD <jeremytanchongdao@gmail.com>
2017-03-18 22:04:57 +0800

    Moved dev.js to tools folder and made a change
    - Added conversion from relative to absolute path for templateToolsDir

--- tools/dev.js
+++ tools/dev.js
@@ -5,13 +5,15 @@ const rimraf = require('rimraf');
 
 const environment = process.env.NODE_ENV;
 const isProduction = environment === 'production';
-
-const templateProjectDir = argv.t;
+var templateToolsDir = argv.t;
+if (!path.isAbsolute(templateToolsDir)) {
+    templateToolsDir = path.join(__dirname, templateToolsDir);
+}
 
 // Start webpack-dev-server
 const webpack = require('webpack');
 const webpackDevServer = require('webpack-dev-server');
-const templateWebpackConfig = require(path.join(templateProjectDir, 'webpack.config.js'));
+const templateWebpackConfig = require(path.join(templateToolsDir, 'webpack.config.js'));
 templateWebpackConfig.entry.bundle.unshift("webpack-dev-server/client?http://localhost:8080/");
 templateWebpackConfig.resolve.modules.unshift(path.join(__dirname, "node_modules"));
 const compiler = webpack(templateWebpackConfig);
@@ -29,20 +31,20 @@ server.listen(8080, "127.0.0.1", function () {
 // Start docfx watcher
 const execFileSync = require('child_process').execFileSync;
 const gaze = require('gaze');
-const templateCopyFiles = require(path.join(templateProjectDir, 'build.copy.js'));
+const templateCopyFiles = require(path.join(templateToolsDir, 'build.copy.js'));
 function docfxBuild(f, curr, previous) {
     rimraf.sync('./bin');
     // Symbolic link used to link index.html to default root object in production
     fs.copySync(path.join(__dirname, 'index.html'), path.join(__dirname, '_site/index.html'));
     templateCopyFiles();
-    execFileSync('docfx', ['build', '-t ' + path.join(templateProjectDir, 'bin')], { stdio: [0, 1, 2] });
+    execFileSync('docfx', ['build', '-t ' + path.join(templateToolsDir, 'bin')], { stdio: [0, 1, 2] });
 }
 
 docfxBuild();
 
 gaze(['templates/**/*', 'plugins/*', 'fonts/*', 'misc/*'],
     {
-        cwd: templateProjectDir
+        cwd: templateToolsDir
     },
     function (err, watcher) {
         watcher.on('all', docfxBuild);
